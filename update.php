686a92e6a14bcbf43540931f2c76bd2eedb8d0e4
krittika-IITB <geniuschanks@gmail.com>
2016-08-05 18:45:22 +0530

    repo link added

--- update.php
+++ update.php
@@ -7,7 +7,7 @@ if( urldecode($_GET['auth']) !== $_SERVER['GITHUB_KRITTIKA_SECRET']){
 	$data = json_decode($payload);
 	var_dump($data->ref);
 	if($data->ref === 'refs/heads/master'){
-		echo shell_exec("sudo -u root /usr/bin/webhook_update.sh https://github.com/elec-club-iitb/website electronics-club");
+		echo shell_exec("sudo -u root /usr/bin/webhook_update.sh https://github.com/krittika-IITB/krittika-IITB.github.io");
 		printf('OK');
 		http_response_code(200);
 	} else {
